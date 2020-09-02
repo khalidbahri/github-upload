49749acb96cd5879ac910af69c8cf0653b81fb66
gitName <a@b.c>
2020-09-02 10:57:33 -0400

    "initial commit - moving project"

---
+++ videowebapp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using videowebapp.Models;
+using Microsoft.ApplicationInsights;
+
+
+namespace videowebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private TelemetryClient aiClient;
+
+        public HomeController(TelemetryClient aiClient)
+        {
+            this.aiClient = aiClient;
+        }
+
+        [HttpPost]
+        public ActionResult Like(string button)
+        {
+            this.aiClient.TrackEvent("LikeClicked");
+            this.aiClient.TrackEvent("SabirJoined");
+            ViewBag.Message = "Thank you for your response";
+          
+            return View("Index");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ videowebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+//adding changes in the source code 
+
+namespace videowebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
